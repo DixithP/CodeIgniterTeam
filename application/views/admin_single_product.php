c9d6ec3a45141b9b43a7ab040b3f3e58fb1d1c02
Mike Hannon(code) <MHCode@Michael-Hannons-MacBook-Pro.local>
2015-05-04 15:30:36 -0700

    Poornima's html

---
+++ application/views/admin_single_product.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');?>
+<html>
+  <head>
+    <!-- <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" /> -->
+    <script type="text/javascript" src="<?php echo base_url("assets/js/jQuery-2.1.3.min.js"); ?>"></script>
+    <!-- <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script> -->
+    <script>
+    $(document).ready(function(){
+
+    });
+
+    </script>
+    <title>Single Product Admin</title>
+  </head>
+	<form action="" method="post">
+		Name:<input type="text" name="product_name"><br>
+		Description:<textarea name='product_description'></textarea><br>
+		Categories :<select name="categories">
+						<option value='shirt'>Shirt</option>
+						<option value='hat'>Hat</option>
+						<option value='mug'>Mug</option>
+						<option value='key chain'>Key Chain</option>
+					</select><br>
+		or Add a new Category : <input type="text" name="new_category"><br>
+		<form  action="" method="post">
+			Images : <input type="submit" value="Upload">
+		</form>
+		<a href=""><img src="random.png" alt=""></a>Image <a href="">Delete</a><input type="checkbox" name="main_image" value="">main<br>
+		<a href=""><img src="random.png" alt=""></a>Image <a href="">Delete</a><input type="checkbox" name="main_image" value="">main<br>
+		<a href=""><img src="random.png" alt=""></a>Image<a href="">Delete</a><input type="checkbox" name="main_image" value="">main<br>
+		<input type="submit" value="cancel" >
+		<input type="submit" value="Preview" >
+		<input type="submit" value="Update" >
+	</form>
+
+</body>
+</html>
